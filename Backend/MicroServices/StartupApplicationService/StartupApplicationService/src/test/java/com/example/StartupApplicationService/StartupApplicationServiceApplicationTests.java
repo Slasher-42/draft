ee598a4747d481c56351227755d8a9c893a3a682
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-03-21 12:32:56 +0200

    scratch

---
+++ Backend/MicroServices/StartupApplicationService/StartupApplicationService/src/test/java/com/example/StartupApplicationService/StartupApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StartupApplicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
