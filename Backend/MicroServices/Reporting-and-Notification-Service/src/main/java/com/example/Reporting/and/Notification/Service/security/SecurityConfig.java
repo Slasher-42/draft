fbf36113b0666c3d68cc18d95f99d9e5c9a8dcbf
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-03-30 22:59:25 +0200

    try

--- Backend/MicroServices/Reporting-and-Notification-Service/src/main/java/com/example/Reporting/and/Notification/Service/security/SecurityConfig.java
+++ Backend/MicroServices/Reporting-and-Notification-Service/src/main/java/com/example/Reporting/and/Notification/Service/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/analytics").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
