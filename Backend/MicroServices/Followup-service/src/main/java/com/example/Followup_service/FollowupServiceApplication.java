6739920a711fe3e9f64005a4afbb23abb48d79a7
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-04-07 14:17:01 +0200

    check

---
+++ Backend/MicroServices/Followup-service/src/main/java/com/example/Followup_service/FollowupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Followup_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowupServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/MicroServices/Followup-service/src/test/java/com/example/Followup_service/FollowupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Followup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
