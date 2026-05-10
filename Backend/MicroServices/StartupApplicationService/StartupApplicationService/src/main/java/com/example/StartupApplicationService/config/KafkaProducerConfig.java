f02625a2dd85ee56d378594f5592872892f41253
Slasher-42 <slasherboy42@gmail.com>
2026-05-10 23:07:33 +0000

    kafka

--- Backend/MicroServices/StartupApplicationService/StartupApplicationService/src/main/java/com/example/StartupApplicationService/config/KafkaProducerConfig.java
+++ Backend/MicroServices/StartupApplicationService/StartupApplicationService/src/main/java/com/example/StartupApplicationService/config/KafkaProducerConfig.java
@@ -24,6 +24,7 @@ public class KafkaProducerConfig {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 5000);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
